e4ef8bfd78c3d2d571b6409f77553127fc46cb98
Vladimir-Fedorov <neroazzurri@mail.ru>
2025-09-17 14:20:03 +0400

    created worker

---
+++ internal/workers/pool.go
@@ -0,0 +1,88 @@
+package workers
+
+import (
+	"packetCutter/internal/domain"
+	"packetCutter/internal/services"
+	"packetCutter/internal/storage"
+	"runtime"
+	"sync"
+)
+
+type WorkerPool struct {
+	matcherService *services.MatcherService
+	collection     *storage.ResultCollection
+	workerCount    int
+	taskCh         chan Task
+	wg             sync.WaitGroup
+	errCh          chan error
+}
+
+func NewWorkerPool(matcherService *services.MatcherService) *WorkerPool {
+	workerCount := runtime.NumCPU()
+	if workerCount < 2 {
+		workerCount = 2
+	}
+
+	wp := &WorkerPool{
+		matcherService: matcherService,
+		collection:     storage.NewResultCollection(),
+		workerCount:    workerCount,
+		taskCh:         make(chan Task, workerCount*10),
+		errCh:          make(chan error, workerCount*100),
+	}
+
+	wp.wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go wp.worker(i)
+	}
+
+	return wp
+}
+
+func (wp *WorkerPool) worker(id int) {
+	defer wp.wg.Done()
+	for task := range wp.taskCh {
+		err := wp.matcherService.ProcessTarget(task.Target, task.Configs)
+		if err != nil {
+			select {
+			case wp.errCh <- err:
+			default:
+				// Пропускаем ошибку если канал переполнен
+			}
+		}
+	}
+}
+
+func (wp *WorkerPool) Submit(target string, configs []domain.SequenceConfig) {
+	wp.taskCh <- Task{Target: target, Configs: configs}
+}
+
+func (wp *WorkerPool) SubmitBatch(targets []string, configs []domain.SequenceConfig) {
+	for _, target := range targets {
+		wp.Submit(target, configs)
+	}
+}
+
+func (wp *WorkerPool) Wait() []error {
+	close(wp.taskCh)
+	wp.wg.Wait()
+	close(wp.errCh)
+
+	errors := make([]error, 0)
+	for err := range wp.errCh {
+		errors = append(errors, err)
+	}
+	return errors
+}
+
+func (wp *WorkerPool) GetCollection() *storage.ResultCollection {
+	return wp.collection
+}
+
+func (wp *WorkerPool) GetStats() map[string]interface{} {
+	return map[string]interface{}{
+		"workers":       wp.workerCount,
+		"results_count": wp.collection.Count(),
+		"unique_keys":   wp.collection.Len(),
+	}
+}
